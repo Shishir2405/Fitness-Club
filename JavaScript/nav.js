2319a1016db76a5f95a6d4f60be1e7e4c721a171
Shishir2405 <Shishirshrivastava30@gmail.com>
2024-03-30 15:51:17 +0530

    stopwatch removed

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -5,11 +5,6 @@ let nav = document.querySelector(".nav");
 svg.addEventListener("click", () => {
   nav.classList.toggle("toggle-nav");
 });
-function checkFullscreen() {
-  if (window.innerWidth < 400) {
-    document.documentElement.requestFullscreen();
-  }
-}
 
 let homeShort = document.querySelector(".name");
 homeShort.addEventListener("click", () => {

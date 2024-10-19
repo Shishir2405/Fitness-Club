e10f0adb12ce53a7965028d50ab9d7676f2a5ab9
Shishir2405 <Shishirshrivastava30@gmail.com>
2024-10-19 11:47:31 +0530

    Bug Fixed

--- JavaScript/nutrition.js
+++ JavaScript/nutrition.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   let input = document.querySelector("#input");
   let productName = document.querySelector(".product");
-  let calories = document.querySelector(".Calories");
   let carbohydrates = document.querySelector(".Carbohydrates");
   let cholestrol = document.querySelector(".Cholestrol");
-  let protein = document.querySelector(".Protein");
   let fiber = document.querySelector(".Fiber");
   let saturatedFat = document.querySelector(".Saturated_fats");
   let totalFat = document.querySelector(".Total_Fat");
   let potassium = document.querySelector(".Potassium");
   let sodium = document.querySelector(".Sodium");
-  let serving = document.querySelector(".serving");
   let sugar = document.querySelector(".sugar");
   let btn = document.querySelector("button");
   let info = document.querySelector(".info");
@@ -39,15 +36,12 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log(response.data);
       productName.innerText = response.data[0].name;
       cholestrol.innerText = response.data[0].cholesterol_mg;
-      calories.innerText = response.data[0].calories;
       carbohydrates.innerText = response.data[0].carbohydrates_total_g;
-      protein.innerText = response.data[0].protein_g;
       fiber.innerText = response.data[0].fiber_g;
       saturatedFat.innerText = response.data[0].fat_saturated_g;
       totalFat.innerText = response.data[0].fat_total_g;
       potassium.innerText = response.data[0].potassium_mg;
       sodium.innerText = response.data[0].sodium_mg;
-      serving.innerText = response.data[0].serving_size_g;
       sugar.innerText = response.data[0].sugar_g;
       head.remove();
     } catch (error) {
